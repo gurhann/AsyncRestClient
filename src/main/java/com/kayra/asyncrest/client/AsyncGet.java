c48841492ae6f3d8d71b82d77585c53d37be282e
Gürhan Küçük <gurhan.kucuk@yandex.com>
2017-01-08 17:30:15 +0300

    added time info.

--- src/main/java/com/kayra/asyncrest/client/AsyncGet.java
+++ src/main/java/com/kayra/asyncrest/client/AsyncGet.java
@@ -1,5 +1,7 @@
 package com.kayra.asyncrest.client;
 
+import java.util.Calendar;
+
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Invocation;
@@ -9,12 +11,13 @@ import javax.ws.rs.core.Response;
 public class AsyncGet {
 
 	public static void main(String [] args) {
+		Calendar calendar = Calendar.getInstance();
 		Client client = ClientBuilder.newClient();
 		WebTarget webTarget = client.target("http://localhost:8080/restsample/async");
 		Invocation.Builder builder = webTarget.request();
-		System.out.println("asdsa");
+		System.out.println("main program started:"+calendar.getTime());
 		Response response = builder.get();
-//		System.out.println(response.readEntity(String.class));
-		System.out.println("asdasdadasdsadasdas");
+		System.out.println(response.readEntity(String.class) + ":" + calendar.getTime());
+		System.out.println("main program ended"+calendar.getTime());
 	}
 }
