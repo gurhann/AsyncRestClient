eeabe78410ead2c1bd4de9ecdd695acce5779677
Gürhan Küçük <gurhan.kucuk@yandex.com>
2017-01-08 12:06:19 +0300

    async get sample.

---
+++ src/main/java/com/kayra/asyncrest/client/AsyncGet.java
@@ -0,0 +1,20 @@
+package com.kayra.asyncrest.client;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Response;
+
+public class AsyncGet {
+
+	public static void main(String [] args) {
+		Client client = ClientBuilder.newClient();
+		WebTarget webTarget = client.target("http://localhost:8080/restsample/async");
+		Invocation.Builder builder = webTarget.request();
+		System.out.println("asdsa");
+		Response response = builder.get();
+//		System.out.println(response.readEntity(String.class));
+		System.out.println("asdasdadasdsadasdas");
+	}
+}
